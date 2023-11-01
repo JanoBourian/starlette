23747604f9db6cafed163c3a7424c92add49b70f
JanoBourian <supermath33@hotmail.com>
2023-10-31 23:56:14 -0600

    Add: more functionalities in requests statements

--- application/application.py
+++ application/application.py
@@ -3,6 +3,7 @@ from starlette.responses import PlainTextResponse, JSONResponse
 from starlette.requests import Request
 from starlette.routing import Route, Mount
 import json
+import time
 
 
 async def application(request: Request) -> JSONResponse:
@@ -18,14 +19,21 @@ async def dummy(request: Request) -> JSONResponse:
     method = request.method
     items = request.items()
     for i in items:
-        print(i[0], i[1])
+        print(i[0], ": ", i[1])
     body = await request.body()
+    stream = request.stream()
+    async for i in stream:
+        print(i)
+    disconnect = await request.is_disconnected()
+    request.state.time_started = time.time()
     return JSONResponse(
         {
             "base_url": base_url,
             "body": json.loads(body.decode()),
             "client": client,
             "method": method,
+            "disconnect": disconnect,
+            "time_started": request.state.time_started
         }
     )
 
